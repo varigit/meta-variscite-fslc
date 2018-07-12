7209f80a72945fb65e328cc5cfd100daa35e2f43
Felix Radensky <felix.r@variscite.com>
2018-07-12 12:55:30 +0300

    imx8m-var-dart: pm-utils: Fix suspend when booting from SD
    Do not execute WIFI suspend script when booting from SD, this causes
    SD errors as SD and WIFI share the same MMC controller.

--- recipes-bsp/pm-utils/pm-utils/imx8m-var-dart/imx8m-wifi.sh
+++ recipes-bsp/pm-utils/pm-utils/imx8m-var-dart/imx8m-wifi.sh
@@ -3,6 +3,12 @@
 . /etc/wifi/variscite-wifi.conf
 . /etc/wifi/variscite-wifi-common.sh
 
+# Exit if WIFI is not available
+if grep -q MX8M /sys/devices/soc0/soc_id && \
+   ! grep -q WIFI /sys/devices/soc0/machine ; then
+        exit 0
+fi
+
 case $1 in
 
 "suspend")
